7f2426214ffc146b7dce68c3bbfd032a4f2df1db
Tyler Danke <tyler@purelypoultry.com>
2015-09-19 14:56:31 -0500

    codeconvoy

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,11 @@
+$(document).ready(function() {
+  /* Add code from the next steps here */
+//  $("#copyright").click(function(){
+//  $(this).css('color','purple');
+//});
+document.getElementById('copyright').onclick = function () {
+   this.style.color = 'purple';
+}
+});
+
 // Your Javascript will go here!
